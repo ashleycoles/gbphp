d97f9f0d6ebef21fccac4ffabb20ebd2b4cfa4fa
Ashley Coles <ashley@acoles.co.uk>
2019-10-23 14:06:51 +0100

    OOP refactoring

--- gbphp-compiler.php
+++ gbphp-compiler.php
@@ -1,147 +1,194 @@
 <?php
 
-$config = loadConfig();
-
-runCompile($config);
-
-/**
- * Runs the compiler
- *
- * @param $config
- */
-function runCompile($config) {
-    if (!$config) {
-        echo 'No config provided';
-        exit;
-    }
-    $files = loadFiles($config);
-
-    foreach ($files as $file) {
-        compile($file, $config);
-    }
+try {
+    $GBPHPCompiler = new GBPHPCompiler();
+    $GBPHPCompiler->runCompiler();
+} catch (Exception $e) {
+    echo $e->getMessage();
+    exit;
 }
 
-/**
- * Compile a given file
- *
- * @param $filepath
- */
-function compile($filepath, $config) {
-    $inputFileInfo = pathinfo($filepath);
-    $outputFilePath = str_replace('./', '/', $inputFileInfo['dirname']) . DIRECTORY_SEPARATOR . $inputFileInfo['filename'] . '.php';
-    $outputFilePath = str_replace($config->input_dir, $config->output_dir, $outputFilePath);
+class GBPHPCompiler {
+    /**
+     * Object holding compiler configuration options.
+     *
+     * Properties are 'input_dir' and 'output_dir'
+     *
+     * @var JSON Object
+     */
+    protected $config;
 
-    $outputFileInfo = pathinfo($outputFilePath);
+    /**
+     * Holds the filepath to the gbphp config file.
+     *
+     * @var string
+     */
+    protected $configPath = 'gbphp-config.json';
 
-    $fileContents = htmlspecialchars(file_get_contents($filepath));
+    /**
+     * Holds the array of files due to be compiled.
+     *
+     * @var Array
+     */
+    protected $filesToCompile;
 
-    replace($fileContents);
-
-    if (!is_dir($outputFileInfo['dirname'])) {
-       mkdir($outputFileInfo['dirname']);
-    }
-
-    file_put_contents($outputFilePath, htmlspecialchars_decode($fileContents));
-}
-
-/**
- * Replace GBPHP with filthy vanilla PHP
- *
- * @param $ephpContents
- */
-function replace(&$ephpContents) {
-    $replacements = [
-        '$'                 => '£',
-        'echo'              => 'announce',
-        'if'                => 'perchance',
-        'else'              => 'otherwise',
-        'switch'            => 'what_about',
-        'case'              => 'perhaps',
-        'default'           => 'on_the_off_chance',
-        'break'             => 'splendid',
-        'try'               => 'would_you_mind',
-        'catch'             => 'actually_i_do_mind',
-        'die'               => 'perish',
-        'exit'              => 'brexit',
-        'Exception'         => 'Wobbly',
-        'foreach'           => 'merry_go_round',
-        'for each'           => 'merry_go_round',
-        // OO
-        'class'             => 'upper_class',
-        'public'            => 'state',
-        'private'           => 'private',
-        'protected'         => 'hereditary',
-        // Built in functions
-        'str_replace'       => 'string_replace',
-        'is_int'            => 'is_integer',
-        'var_dump'          => 'variable_dump',
-        'preg_match'        => 'perl_regular_expression_match',
-        'json_encode'       => 'javascript_object_notation_encode',
-        'json_decode'       => 'javascript_object_notation_decode',
-        'mysql_connect'     => 'my_structured_query_language_connect',
-        'mysqli_connect'    => 'my_structured_query_language_improved_connect',
-        'setcookie'        => 'serve_biscuit',
-        // Globals
-        '_COOKIE'           => '_BISCUIT',
-        '_POST'             => '_ROYAL_MAIL',
-        '_SERVER'           => '_BUTLER',
-        // Other
-        'php'               => 'gbphp',
-    ];
-
-    foreach ($replacements as $replacement => $search) {
-        $ephpContents = str_replace_outside_quotes($search, $replacement, $ephpContents);
-    }
-}
-
-/**
- * Load all gbphp files in the input directory
- *
- * @param $config
- * @return array
- */
-function loadFiles($config) {
-    $iterator = new RecursiveDirectoryIterator($config->input_dir);
-    $files = [];
-    foreach (new RecursiveIteratorIterator($iterator) as $file)
+    /**
+     * GBPHPCompiler constructor.
+     *
+     * @throws Exception
+     */
+    public function __construct()
     {
-        $file_bits = explode('.', $file);
-        if (in_array(strtolower(array_pop($file_bits)), ['gbphp'])) {
-            $files[] = $file->getPathname();
+        $this->loadConfig();
+
+    }
+
+    /**
+     * Runs the GBPHP compiler
+     */
+    public function runCompiler()
+    {
+        $this->loadFiles();
+
+        foreach ($this->filesToCompile as $file) {
+            $this->compile($file);
         }
     }
-    return $files;
-}
 
-/**
- * Wrapper for str_replace() to only replace outside of single or double quotes
- *
- * @param $replace
- * @param $with
- * @param $string
- * @return string
- */
-function str_replace_outside_quotes($replace, $with, $string){
-    $result = "";
-    $outside = preg_split('/("[^"]*"|\'[^\']*\')/',$string,-1,PREG_SPLIT_DELIM_CAPTURE);
-    while ($outside)
-        $result .= str_replace($replace,$with,array_shift($outside)).array_shift($outside);
-    return $result;
-}
+    /**
+     * Loads the compiler config file
+     *
+     * @throws Exception
+     */
+    protected function loadConfig()
+    {
+        if (!file_exists($this->configPath)) {
+            throw new Exception("Error: Config file not found.");
+        }
 
-/**
- * Loads GBPHP config from json file
- *
- * @return JSON Object
- */
-function loadConfig() {
-    $configName = 'gbphp-config.json';
+        $jsonString = file_get_contents($this->configPath);
+        $this->config = json_decode($jsonString);
 
-    if (!file_exists($configName)) {
-        return false;
+        return $this;
     }
 
-    $jsonString = file_get_contents($configName);
-    $config = json_decode($jsonString);
-    return $config;
+    /**
+     * Recurisve glob through specified input_dir to generate an array of .gbphp files to be compiled
+     *
+     * @throws Exception
+     */
+    protected function loadFiles()
+    {
+        if (!$this->config) {
+            throw new Exception('Error: Config not loaded.');
+        }
+
+        $iterator = new RecursiveDirectoryIterator($this->config->input_dir);
+
+        foreach (new RecursiveIteratorIterator($iterator) as $file)
+        {
+            $file_bits = explode('.', $file);
+            if (in_array(strtolower(array_pop($file_bits)), ['gbphp'])) {
+                $this->filesToCompile[] = $file->getPathname();
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Run the compiler
+     *
+     * @param $filepath string pointing to the .gbphp file due to be compiled.
+     */
+    protected function compile($filepath)
+    {
+        $inputFileInfo = pathinfo($filepath);
+        $outputFilePath = str_replace('./', '/', $inputFileInfo['dirname']) . DIRECTORY_SEPARATOR . $inputFileInfo['filename'] . '.php';
+        $outputFilePath = str_replace($this->config->input_dir, $this->config->output_dir, $outputFilePath);
+
+        $outputFileInfo = pathinfo($outputFilePath);
+
+        $fileContents = htmlspecialchars(file_get_contents($filepath));
+
+        $this->replace($fileContents);
+
+        if (!is_dir($outputFileInfo['dirname'])) {
+            mkdir($outputFileInfo['dirname']);
+        }
+
+        file_put_contents($outputFilePath, htmlspecialchars_decode($fileContents));
+    }
+
+    /**
+     * Runs search and replaces against a .gbphp to 'compile' raw PHP
+     *
+     * @param string $gbphpContents contents of a .gbphp file for compilation
+     * @return $this
+     */
+    protected function replace(string &$gbphpContents)
+    {
+        $replacements = [
+            '$'                 => '£',
+            'echo'              => 'announce',
+            'if'                => 'perchance',
+            'else'              => 'otherwise',
+            'switch'            => 'what_about',
+            'case'              => 'perhaps',
+            'default'           => 'on_the_off_chance',
+            'break'             => 'splendid',
+            'try'               => 'would_you_mind',
+            'catch'             => 'actually_i_do_mind',
+            'die'               => 'perish',
+            'exit'              => 'brexit',
+            'Exception'         => 'Wobbly',
+            'foreach'           => 'merry_go_round',
+            'for each'           => 'merry_go_round',
+            // OO
+            'class'             => 'upper_class',
+            'public'            => 'state',
+            'private'           => 'private',
+            'protected'         => 'hereditary',
+            // Built in functions
+            'str_replace'       => 'string_replace',
+            'is_int'            => 'is_integer',
+            'var_dump'          => 'variable_dump',
+            'preg_match'        => 'perl_regular_expression_match',
+            'json_encode'       => 'javascript_object_notation_encode',
+            'json_decode'       => 'javascript_object_notation_decode',
+            'mysql_connect'     => 'my_structured_query_language_connect',
+            'mysqli_connect'    => 'my_structured_query_language_improved_connect',
+            'setcookie'         => 'serve_biscuit',
+            // Globals
+            '_COOKIE'           => '_BISCUIT',
+            '_POST'             => '_ROYAL_MAIL',
+            '_SERVER'           => '_BUTLER',
+            // Other
+            'php'               => 'gbphp',
+        ];
+
+        foreach ($replacements as $replacement => $search) {
+            $gbphpContents = $this->stringReplaceOutsideQuotes($search, $replacement, $gbphpContents);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Wrapper function for str_replace to only replace strings outside of quote characters.
+     *
+     * @param string $search
+     * @param string $replace
+     * @param string $source
+     * @return string
+     */
+    protected function stringReplaceOutsideQuotes(string $search,string $replace,string $source): string
+    {
+        $result = "";
+        $outside = preg_split('/("[^"]*"|\'[^\']*\')/',$source,-1,PREG_SPLIT_DELIM_CAPTURE);
+        while ($outside) {
+            $result .= str_replace($search,$replace,array_shift($outside)).array_shift($outside);
+        }
+        return $result;
+    }
 }
